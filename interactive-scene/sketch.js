a08a5d36bec9b05c939f494cf96ce37e9f0cf3a9
Angad Chahal <angadchahal.210@gmail.com>
2024-09-20 11:02:35 -0600

    start of interactive scene

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -4,13 +4,12 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+let screenChicken = loadImage("/pictures/chicken1");
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
-  circle(mouseX, mouseY, 30);
+  image(screenChicken);
 }

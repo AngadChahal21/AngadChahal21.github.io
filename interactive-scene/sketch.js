dcf1f4ae1f2aab9de91ef5c61450bbfea38764bc
Angad Chahal <angadchahal.210@gmail.com>
2024-09-23 10:34:04 -0600

    Image Demo

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -1,13 +1,18 @@
 // Project Title
 // Your Name
 // Date
-// A replica for a 
+// A replica for a game called "OKAY?"
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+let height = 220;
+let width = 30;
+
 function draw() {
-  image(screenChicken);
+  background(21,97,109);
+  fill(0,21,36);
+  rect(width/2,height/2, width, height);
 }

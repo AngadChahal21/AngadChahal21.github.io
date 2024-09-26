972429e93e6660a835ae73a3d0c3ed3f9c8a096d
Angad Chahal <angadchahal.210@gmail.com>
2024-09-26 10:25:28 -0600

    Interactive scene commit 3

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -4,14 +4,21 @@
 // A replica for a game called "OKAY?"
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
+console.log("check");
 let rectHeight = 220;
 let rectWidth = 30;
 let speed = 3;
 let y =  rectHeight*1.33;
 let radius = 40;
+
+let x1;
+let y1;
+let x2;
+let y2;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
 
 
 //1st rectangle
@@ -21,19 +28,22 @@ let upperBound1 = height/2 - rectHeight*1.33;
 let lowerBound2 = height/2 + rectHeight/3 + rectHeight;
 
 function draw() {
-  
   background(21,97,109);
-  fill(0,21,36);
   
-  //osbtacle rectangles 
+  //osbtacle rectangles
+  fill(0,21,36);
   rect(width/2,height/2 + rectHeight/3  , rectWidth, rectHeight); // 2nd rectangle
   rect(width/2,height/2 - rectHeight*1.33   , rectWidth, rectHeight); // 1strectangle
+
+  //hitting rectangle
+  fill(255, 236, 209);
+  rect(width/2 - 150 ,height/2 + rectHeight/3  , rectWidth * 2, rectHeight); // 2nd rectangle
 
   //moving circle
   fill(255, 236, 209);
   moveBall();
   changeDirection();
-
+  slingshot();
   // if(y > lowerBound2 || y< upperBound1){
   //   speed = speed * -1; 
   // }
@@ -52,5 +62,17 @@ function changeDirection(){
   // }
   if (y >= height - radius || y <= 0 + radius) {
     speed = speed * -1;
+  }
+}
+
+function slingshot(){
+  function mouseClicked(){
+    x1 = mouseX;
+    y1 = mouseY;
+    console.log(x1, y1);
+  }
+  function mouseReleased(){
+    x2 = mouseX;
+    y2 = mouseY;
   }
 }

483dd12d8cdedde588364e830648adfda80c10d5
Angad Chahal <angadchahal.210@gmail.com>
2024-10-08 11:03:40 -0600

    interesting Potential ideas and elements added

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -5,6 +5,20 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+/*Ideas:
+Patterns 
+Generative art 
+A complilations of generative games 
+
+Elements that can be used:
+https://www.reddit.com/r/p5js/comments/1fwsv9t/charged/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+https://www.reddit.com/r/p5js/comments/1frnncu/futuristic_shape_generator/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+https://www.reddit.com/r/p5js/comments/1ed3gci/infinite_donuts_interactive_website/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+(can be used for a maze game)https://www.reddit.com/r/p5js/comments/1eazmb9/amazing/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+(collision library for p5 js) https://github.com/bmoren/p5.collide2D.git
+https://www.reddit.com/r/p5js/comments/1e2q5i7/brightness_based_ascii_renderer_including_edge/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+https://www.reddit.com/r/p5js/comments/1djme24/ive_been_addicted_to_playing_bitburner_lately/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+*/
 
 function setup() {
   createCanvas(windowWidth, windowHeight);

f1197ba066ade46202cbfb888899721e848b0953
Angad Chahal <angadchahal.210@gmail.com>
2024-10-09 11:03:26 -0600

    research on maze game and starting
    Resources of research pasted into the top comments and just started the start screen

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -20,10 +20,21 @@ https://www.reddit.com/r/p5js/comments/1e2q5i7/brightness_based_ascii_renderer_i
 https://www.reddit.com/r/p5js/comments/1djme24/ive_been_addicted_to_playing_bitburner_lately/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
 */
 
+
+/*
+Maze generation using Prim's  algorithm https://www.youtube.com/watch?v=BxabnKrOjT0      https://github.com/ian-howell/Mazes
+Maze solution using Depth First Search algorithm https://medium.com/swlh/solving-mazes-with-depth-first-search-e315771317ae
+
+*/
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
+}
+
+function startScreen(){
+  background(0);
+  text("Maze Mania", width/2, height/2); 
 }

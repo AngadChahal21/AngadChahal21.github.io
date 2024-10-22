ec072c65e575f3eb5b438f11b6278f65f4664a86
Angad Chahal <angadchahal.210@gmail.com>
2024-10-22 11:02:45 -0600

    grid demo

---
+++ grid/sketch.js
@@ -0,0 +1,88 @@
+
+// let grid = [[1, 0, 0, 1],
+//             [0, 1, 1, 0],
+//             [0, 0, 1, 1],
+//             [1, 1, 1, 0]];
+
+let grid;            
+let cellSize;
+const GRID_SIZE = 10;
+
+function setup() {
+
+  if(windowWidth< windowHeight){
+    createCanvas(windowWidth, windowWidth);
+  }
+  else{
+    createCanvas(windowHeight, windowHeight);
+  }
+  cellSize = height/GRID_SIZE;
+  grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+}
+
+function draw() {
+  frameRate(15);
+  background(220);
+  displayGrid();
+}
+
+function keyPressed(){
+  if(key === "r"){
+    grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  }
+
+  if(key === "e"){
+    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  }
+}
+
+function displayGrid(){
+  for(let y = 0; y < GRID_SIZE; y++){
+    for(let x = 0; x < GRID_SIZE; x++){
+      if(grid[y][x] === 1){
+        fill(0);
+      }
+      else if(grid[y][x] === 0){
+        fill(255);
+      }
+
+      square(x * cellSize,y * cellSize, cellSize);
+
+      if(mouseIsPressed && mouseX < x * cellSize + cellSize && mouseX > x * cellSize && mouseY < y * cellSize + cellSize && mouseY > y * cellSize){
+        grid[y][x] =  Math.abs(grid[y][x] - 1);
+      }
+    }
+  }
+}
+
+
+function generateRandomGrid(cols,rows){
+  let newGrid = [];
+  for(let y = 0; y < rows; y++){
+    newGrid.push([]);
+    for(let x = 0; x < cols; x++){
+      //make it a 1 half the time, a 0 haft the time
+      if(random(100) < 50){
+        newGrid[y].push(1);
+      }
+
+      else{
+        newGrid[y].push(0);
+      }
+    }
+  }
+  return newGrid;
+  console.log(newGrid);
+}
+
+function generateEmptyGrid(cols, rows){
+  let newGrid = [];
+  for(let y = 0; y < rows; y++){
+    newGrid.push([]);
+    for(let x = 0; x < cols; x++){
+      newGrid[y].push(0);
+    }
+  }
+  return newGrid;
+  console.log(newGrid);
+}
